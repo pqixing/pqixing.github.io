a060d8b7697442529b284a2a20331e58ceb867ed
qXp <pqixing@163.com>
2019-05-13 12:24:27 +0800

    Update bookmark.js
    click

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,6 +1,6 @@
  function closeBookMark(b){
                var c=b.nextElementSibling
-				console.log("Close"+c.tagName)
+				console.log("Click"+c.tagName)
 				if(c.style.display=="none"){
 				c.style.display="block"
 				}else c.style.display="none"
