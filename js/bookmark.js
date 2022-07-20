cf8521ed9ed96bbb56f4d02de8bfbe5d655dbd5e
pengqixing <pqixing@163.com>
2022-07-20 16:20:15 +0800

    使用flutter

--- js/bookmark.js
+++
@@ -1,8 +0,0 @@
- function closeBookMark(b){
-               var c=b.nextElementSibling
-				console.log("Click"+c.tagName)
-				if(c.style.display=="none"){
-				c.style.display="block"
-				}else c.style.display="none"
-
-			}
